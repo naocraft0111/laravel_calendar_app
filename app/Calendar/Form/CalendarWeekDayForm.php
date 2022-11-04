afd30aa712604f21a609247b7f3cf1d0a9411dd1
naocraft0111 <naoki1306@gmail.com>
2022-11-04 17:28:46 +0900

    修正

--- app/Calendar/Form/CalendarWeekDayForm.php
+++ app/Calendar/Form/CalendarWeekDayForm.php
@@ -24,11 +24,11 @@ class CalendarWeekDayForm extends CalendarWeekDay {
         // 定休日設定の値
         $defaultValue = ($this->isHoliday) ? "休み" : "営業日";
         // 臨時休業が設定されているかどうか
-        $isSelectedExtraClose = ($this->extraHoliday && $this->extraHoliday->isClose() ? 'selected' : '');
+        $isSelectedExtraClose = ($this->extraHoliday && $this->extraHoliday->isClose()) ? 'selected' : '';
         // 臨時営業が設定されているかどうか
-        $isSelectedExtraOpen = ($this->extraHoliday && $this->extraHoliday->isOpen() ? 'selected' : '');
+        $isSelectedExtraOpen = ($this->extraHoliday && $this->extraHoliday->isOpen()) ? 'selected' : '';
         // コメントの値
-        $comment = ($this->extraHoliday ? $this->extraHoliday->comment : '');
+        $comment = ($this->extraHoliday) ? $this->extraHoliday->comment : '';
 
         // HTMLの組み立て
         $html = [];
@@ -37,14 +37,14 @@ class CalendarWeekDayForm extends CalendarWeekDay {
         $html[] = '<p class="day">' . $this->carbon->format("j"). '</p>';
         // 臨時営業・臨時休業設定
         $html[] = '<select name="'. $select_form_name . '" class="form-control">';
-        $html[] = '<option value="0">- (' . $defaultValue . ')</option>';
-        $html[] = '<option value="'.ExtraHoliday::CLOSE.'" '. $isSelectedExtraClose . '>臨時休業</option>';
-        $html[] = '<option value="'.ExtraHoliday::OPEN.'" '. $isSelectedExtraOpen . '>臨時営業</option>';
-        $html[] = '</select>';
+		$html[] = '<option value="0">- (' . $defaultValue . ')</option>';
+		$html[] = '<option value="'.ExtraHoliday::CLOSE.'" ' . $isSelectedExtraClose . '>臨時休業</option>';
+		$html[] = '<option value="'.ExtraHoliday::OPEN.'" ' . $isSelectedExtraOpen . '>臨時営業</option>';
+		$html[] = '</select>';
         // コメント
         if($isSelectedExtraClose || $isSelectedExtraOpen){
-            $html[] = '<input class="form-control" type="text"' .$comment_form_name.'" value="'.e($comment).'" />';
-        }
+			$html[] = '<input class="form-control" type="text" name="'.$comment_form_name.'" value="'.e($comment).'" />';
+		}
 
         return implode("", $html);
     }
