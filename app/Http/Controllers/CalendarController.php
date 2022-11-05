d0c0cce1b10a01d76dbf5e6e989ad484adccf455
naocraft0111 <naoki1306@gmail.com>
2022-11-05 13:53:36 +0900

    クエリー方式で前後の月を取得

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -8,8 +8,21 @@ use App\Calendar\Output\CalendarOutputView;
 
 class CalendarController extends Controller
 {
-    public function show(){
-        $calendar = new CalendarOutputView(time());
+    public function show(Request $request){
+
+        // クエリーのdateを受け取る
+		$date = $request->input("date");
+
+		// dateがYYYY-MMの形式かどうか判定する
+		if($date && preg_match("/^[0-9]{4}-[0-9]{2}$/", $date)){
+			$date = strtotime($date . '+1 month');
+		}else{
+			$date = null;
+		}
+
+		// 取得出来ない時は現在(=今月)を指定する
+		if(!$date)$date = time();
+        $calendar = new CalendarOutputView($date);
 
         return view('calendar', compact('calendar'));
     }
